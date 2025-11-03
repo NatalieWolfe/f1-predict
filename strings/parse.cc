20ef526c951eaca126485c8c49476826997f7780
Natalie Wolfe <natalie@lifewanted.com>
2025-11-02 17:08:33 -0800

    Add tests for string parsing

--- strings/parse.cc
+++ strings/parse.cc
@@ -22,35 +22,31 @@ bool from_chars(std::string_view str, T& val) {
 } // namespace
 
 milliseconds parse_duration(std::string_view duration_str) {
-  std::vector<std::string_view> chunks;
-  chunks.reserve(3);
-  uint64_t pos = 0;
-  for (uint64_t i = 0; i < duration_str.size(); ++i) {
-    if (duration_str[i] == ':') {
-      chunks.push_back(duration_str.substr(pos, i - pos));
-      pos = i + 1;
-    }
-  }
-  if (pos < duration_str.size() - 1) chunks.push_back(duration_str.substr(pos));
-
   milliseconds duration{0};
-  switch (chunks.size()) {
-    case 3: { // Hours
-      int64_t val;
-      if (from_chars(chunks[0], val)) duration += std::chrono::hours{val};
-      [[fallthrough]];
-    }
-    case 2: { // Minutes
-      int i = chunks.size() - 2;
-      int64_t val;
-      if (from_chars(chunks[i], val)) duration += std::chrono::minutes{val};
-      [[fallthrough]];
-    }
-    case 1: { // Seconds
-      int i = chunks.size() - 1;
-      double val;
-      if (from_chars(chunks[i], val)) {
-        duration += milliseconds{static_cast<int64_t>(val * 1000)};
+  int chunk_count = 0;
+  int prev_chunk_start = duration_str.size();
+  for (int64_t i = static_cast<int64_t>(duration_str.size()) - 1; i >= 0; --i) {
+    if (i > 0 && duration_str[i] != ':') continue;
+
+    std::string_view chunk = duration_str.substr(i, prev_chunk_start - i);
+    if (chunk[0] == ':') chunk = chunk.substr(1);
+    switch (++chunk_count) {
+      case 3: { // Hours
+        int64_t val;
+        if (from_chars(chunk, val)) duration += std::chrono::hours{val};
+        break;
+      }
+      case 2: { // Minutes
+        int64_t val;
+        if (from_chars(chunk, val)) duration += std::chrono::minutes{val};
+        break;
+      }
+      case 1: { // Seconds
+        double val;
+        if (from_chars(chunk, val)) {
+          duration += milliseconds{static_cast<int64_t>(val * 1000)};
+        }
+        break;
       }
     }
   }
