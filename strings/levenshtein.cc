d83caba19c0dc2a81d0e68d5dc9546f1bdf7cfc8
Natalie Wolfe <natalie@lifewanted.com>
2025-11-13 13:00:32 -0800

    Use character count, not byte size for levenshtein

--- strings/levenshtein.cc
+++ strings/levenshtein.cc
@@ -9,23 +9,23 @@ namespace f1_predict {
 int levenshtein_distance(std::string_view a, std::string_view b) {
   // Create a 2D vector (matrix) to store the distances
   std::vector<std::vector<int>> distances(
-      a.size() + 1, std::vector<int>(b.size() + 1));
+      a.length() + 1, std::vector<int>(b.length() + 1));
 
   // Initialize the first row and column
   // distances[i][0] represents the distance to transform an empty string to the
   // first i characters of a (i deletions)
-  for (size_t i = 0; i <= a.size(); ++i) {
+  for (size_t i = 0; i <= a.length(); ++i) {
     distances[i][0] = i;
   }
   // distances[0][j] represents the distance to transform the first j characters
   // of b to an empty string (j insertions)
-  for (size_t j = 0; j <= b.size(); ++j) {
+  for (size_t j = 0; j <= b.length(); ++j) {
     distances[0][j] = j;
   }
 
   // Fill the rest of the matrix
-  for (size_t i = 1; i <= a.size(); ++i) {
-    for (size_t j = 1; j <= b.size(); ++j) {
+  for (size_t i = 1; i <= a.length(); ++i) {
+    for (size_t j = 1; j <= b.length(); ++j) {
       if (a[i - 1] == b[j - 1]) {
         // Characters match, no operation needed for this step
         distances[i][j] = distances[i - 1][j - 1];
